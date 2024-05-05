60cec5bcbc5f94cb10bbb0affa4a8925de9ab80f
l3xst1ng <howdy.martt@gmail.com>
2024-05-05 14:34:56 +0300

    Added botscards jsx

--- src/BotsCards.js
+++ src/BotsCards.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./components/BotsCards.css";
 
-function BotsCards({ bot, botlist, botdetails }) {
-  return <div>BotsCards</div>;
+function BotsCards({ bot, botList, botDetails }) {
+  return (
+    <div>
+      <img src={bot.avatar_url} alt={bot.name} />
+      <h3>{bot.name}</h3>
+      <p>{bot.catchphrase}</p>
+      <button onClick={() => botList(bot)}>Enlist</button>
+      <button onClick={() => botDetails(bot)}>Show Specs</button>
+    </div>
+  );
 }
 
 export default BotsCards;
