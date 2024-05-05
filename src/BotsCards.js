ed3662c229430f801a45500e8f7a8bfcbfd4baba
l3xst1ng <howdy.martt@gmail.com>
2024-05-05 14:18:00 +0300

    Added Botcards component with props

---
+++ src/BotsCards.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./components/BotsCards.css";
+
+function BotsCards({ bot, botlist, botdetails }) {
+  return <div>BotsCards</div>;
+}
+
+export default BotsCards;
